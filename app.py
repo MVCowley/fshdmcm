ac3f18ad380fecf004d0450f35fe39a824856fea
MVCowley <51127523+MVCowley@users.noreply.github.com>
2024-08-07 08:57:51 +0100

    feat(ui): add tabs

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ FIELDS = fields.get_field_strings()
 
 ui.page_opts(fillable=True, title="FSHD Markov Chain Model")
 
+
 with ui.sidebar():
     ui.input_selectize(
         id="param",
@@ -65,41 +66,44 @@ with ui.sidebar():
     ui.input_action_button("reset", "Reset sliders")
     ui.input_dark_mode()
 
-"Calculated myonuclear lifetime"
+with ui.navset_card_tab(id="tab"):
+    with ui.nav_panel("Myonuclear Lifetime"):
 
-with ui.card(full_screen=True):
+        with ui.card(full_screen=True):
 
-    @render.plot
-    def mc_plot():
-        real_params = [
-            (0.00211) * 10 ** input.vd(),
-            (0.246) * 10 ** input.d0(),
-            (6.41) * 10 ** input.vt(),
-            (1 / 13) * 10 ** input.td(),
-            (1 / 20.2) * 10 ** input.dr(),
-            (0.04023596) * 10 ** input.delta(),
-        ]
+            @render.plot
+            def mc_plot():
+                real_params = [
+                    (0.00211) * 10 ** input.vd(),
+                    (0.246) * 10 ** input.d0(),
+                    (6.41) * 10 ** input.vt(),
+                    (1 / 13) * 10 ** input.td(),
+                    (1 / 20.2) * 10 ** input.dr(),
+                    (0.04023596) * 10 ** input.delta(),
+                ]
 
-        values, omega_s = single_markov_chain.calc_array(
-            input.param(), real_params, -4, 4
-        )
+                values, omega_s = single_markov_chain.calc_array(
+                    input.param(), real_params, -4, 4
+                )
 
-        colours = {field: f"C{n}" for n, field in enumerate(FIELDS)}
+                colours = {field: f"C{n}" for n, field in enumerate(FIELDS)}
 
-        fig, ax = plt.subplots(figsize=(3, 3))
-        ax.plot(values, omega_s, c=colours[input.param()])
+                subplots = plt.subplots(figsize=(3, 3))
+                fig: plt.Figure = subplots[0]
+                ax: plt.Axes = subplots[1]
+                ax.plot(values, omega_s, c=colours[input.param()])
 
-        ax.set_xscale("log")
+                ax.set_xscale("log")
 
-        ax.set_xlabel(f"Fold change in {input.param()}")
-        ax.set_ylabel("Fold change in myonuclear lifetime")
+                ax.set_xlabel(f"Fold change in {input.param()}")
+                ax.set_ylabel("Fold change in myonuclear lifetime")
 
-        ax.set_xlim(1e-4, 1e4)
-        fig.tight_layout()
+                ax.set_xlim(1e-4, 1e4)
+                fig.tight_layout()
 
 
-@reactive.effect
-@reactive.event(input.reset)
-def _():
-    for param in ["vd", "d0", "vt", "td", "dr", "delta"]:
-        ui.update_slider(param, value=0)
+        @reactive.effect
+        @reactive.event(input.reset)
+        def _():
+            for param in ["vd", "d0", "vt", "td", "dr", "delta"]:
+                ui.update_slider(param, value=0)
